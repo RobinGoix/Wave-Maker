e9d454fd22e34fe38f05fd2cc263abf1e9c85365
Robin Goix <robin.goix@free.fr>
2014-04-30 17:01:25 +0200

    Changed weak formulation.

--- Clean Codes/SWEObjectReferential.py
+++ Clean Codes/SWEObjectReferential.py
@@ -8,7 +8,7 @@ from dolfin import *
 
 #Mesh discretization
 Ny = 32
-Nx = 32
+Nx = 64
 
 #Physical values for the physical problem
 g = 9.8 #Gravity [m.s^(-2)]
@@ -35,7 +35,7 @@ c0 = (h0*g)**(0.5)
 epsilon = a0/h0
 
 #Other Parameters
-save = True
+save = False
 ploting = True
 
 #Scaled parameters to solve the dimensionless problem
@@ -53,8 +53,8 @@ hd = hd/h0 #depth
 ad = ad/a0 #height of the moving object
 
 #Define the profil of the moving seabed
-vmax = 0.5*(hd*g)**(0.5) #Speed
-U = vmax/c0 #Rescaled speed
+vmax = (hd*g)**(0.5) #Speed
+U = a0/(c0*h0)*vmax #Rescaled speed
 vObject = Expression(("U","0.0"), U=U, c0=c0)
 seabed = 'hd - 0.5*lambda0/10.*(x[1]>2./lambda0 ? 1. : 0.)*(x[1]-2./lambda0) + 0.5*lambda0/10.*(x[1]<(-2./lambda0) ? 1. : 0.)*(x[1]+2./lambda0)'
 movigObject = ' - (x[1]>0 ? 1. : 0.)*epsilon*ad*0.5*0.5*(1. - tanh(lambda0*x[1]-2.))*(tanh(10*(1. - lambda0*x[0])) + tanh(lambda0*x[0] + 1)) ' \
@@ -80,7 +80,7 @@ def NoSlip_boundary(x, on_boundary):
         return on_boundary and \
                (x[1] < y0 + DOLFIN_EPS or x[1] > y1 - DOLFIN_EPS or \
                 x[0] < x0 + DOLFIN_EPS or x[0] > x1 - DOLFIN_EPS)
-No_Slip0 = DirichletBC(E.sub(0),  Expression(("-U/epsilon","0.0"), U=U, epsilon=epsilon, c0=c0), NoSlip_boundary)
+No_Slip0 = DirichletBC(E.sub(0),  Expression(("-2*U","0.0"), U=U), NoSlip_boundary)
 
 def Entry_boundary(x, on_boundary):
         return on_boundary and x[0] > x1 - DOLFIN_EPS
@@ -90,7 +90,7 @@ bc = [No_Slip0,No_Slip1]
 n=FacetNormal(Th) #Normal Vector
 
 ###############DEFINITION OF THE WEAK FORMULATION############
-u_0 = Expression(("-U/epsilon", "0.0"), U=U, epsilon=epsilon,c0=c0) #Initialisation of the velocity
+u_0 = Expression(("-2*U", "0.0"), U=U) #Initialisation of the velocity
 eta_0 = Expression("0.0") #Initialisation of the free surface
 
 w_prev = Function(E)
@@ -110,12 +110,11 @@ u,eta = as_vector((w[0],w[1])),w[2]
 wt = TestFunction(E)
 v,xi = as_vector((wt[0],wt[1])),wt[2]
 
-F = 1./dt*inner(u-u_prev,v)*dx + inner(grad(u)*(epsilon*u-vObject),v)*dx \
-    - div(v)*eta*dx
+F = 1./dt*inner(u-u_prev,v)*dx + inner(grad(u)*(epsilon*u),v)*dx \
+    + inner(v,grad(eta))*dx
 
-F += 1./dt*(eta-eta_prev)*xi*dx + div((h+epsilon*eta)*(u-vObject/epsilon))*xi*dx
-     
-    
+F += 1./dt*(eta-eta_prev)*xi*dx + div((h+epsilon*eta)*u)*xi*dx
+
 w_ = Function(E)
 (u_, eta_) = w_.split()
 F = action(F, w_)   
